cb21a025ccd5a4f82711d3d444cdb1c1eba90519
IgorChupryna <ihor.chupryna@ukrsibbank.com>
2017-10-17 21:01:43 +0300

    Delete IMenuDao.java

--- src/main/java/IMenuDao.java
+++
@@ -1,8 +0,0 @@
-
-import sun.net.www.content.text.Generic;
-
-import java.util.List;
-
-public interface IMenuDao<I extends Number, M extends BaseEntity> extends GenericDao<Integer,Menu> {
-    List findOrdersSubmittedSince(Double weight);
-}
